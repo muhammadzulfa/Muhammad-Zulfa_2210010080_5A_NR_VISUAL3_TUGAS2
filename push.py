3c8a6bde7e44cccb61bf1261607d5afad8627c05
Zulfan <62686312+muhammadzulfa@users.noreply.github.com>
2024-10-25 17:26:08 +0800

    Penggunaan Element Push

---
+++ push.py
@@ -0,0 +1,23 @@
+import PySimpleGUI as sg
+
+susunan = [
+    [
+        sg.Push(), 
+        sg.Text("UNISKA MAAB", font=("Helvetica", 24)), 
+        sg.Push()
+    ],
+    [
+        sg.Push(), 
+        sg.Text("BANJARMASIN", font=("Courier", 18)), 
+        sg.Push()
+    ]
+]
+
+window = sg.Window(
+    title="Elemen Text", 
+    layout=susunan, 
+    size=(430, 200)
+)
+
+window()
+window.close()
